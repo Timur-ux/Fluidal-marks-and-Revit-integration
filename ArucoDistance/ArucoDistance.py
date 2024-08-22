work on correct rotation

ArucoDistance/ArucoDistance.py
@@ -4,79 +4,26 @@ from pupil_apriltags import Detector
 import time
 from CameraCalibrator import calibrateMarkerSize
 
-
-marker_size = 0.0724
-def processImage(img, at_detector, camera_matrix, dist_coeffs, camera_fc_params):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cellSize = (int(img.shape[1]/10), int(img.shape[0]/10))
-    
-    detectionResult = at_detector.detect(
-        gray, estimate_tag_pose=True, camera_params=camera_fc_params, tag_size=marker_size)
-
-    ids = []
-    corners = []
-    rvecs = []
-    tvecs = []
-    for result in detectionResult:
-        ids.append(result.tag_id)
-        corners.append(result.corners)
-        rvecs.append(result.pose_R)
-        tvecs.append(result.pose_t)
-
-    if ids != []:
-        for i in range(len(ids)):
-            # Calculate distance
-            distance = np.linalg.norm(tvecs[i])
-
-            cv2.drawFrameAxes(img, camera_matrix, dist_coeffs,
-                              rvecs[i], tvecs[i], marker_size * 0.5)
-            cv2.putText(img, f"Dist: {distance:.5f}m z: {tvecs[i][2][0]:.5f}", (int(cellSize[0]*1.5), cellSize[1]), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 255, 0), 5, cv2.LINE_AA)
-            cv2.putText( img, f"Id: {ids[i]}", (cellSize[0]*2, cellSize[1]*2), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 255, 0), 5, cv2.LINE_AA)
-
-    img = cv2.resize(img, (1600, 800))
-    cv2.imshow('Frame', img)
-    cv2.waitKey(0)
+import DrawFunctions as DF
+from utils import CameraInfo
 
 
-def startRecognize(camera_config, source, recognitionProcessor, precalibrateMarkerSizeData = None):
-    """
-        source is a camera id or file's name is containing video
-        source may be also image file
-        if precalibrateMarkerSizeData as (img with marker, dist to marker, real marker size) is given, marker size will be precalibrated
-    """
-    
-    camera_matrix = np.asarray(camera_config["cameraMatrix"])
-    dist_coeffs = np.asarray(camera_config["distCoeffs"])
-    camera_fc_params = np.asarray([camera_config["fx"], camera_config["fy"],
-                        camera_config["cx"], camera_config["cy"]])
-    at_detector = Detector(
-        families="tagStandard41h12",
-        nthreads=4,
-        quad_decimate=1.0,
-        quad_sigma=0.0,
-        refine_edges=1,
-        decode_sharpening=0.25,
-        debug=0
-    )
-    if precalibrateMarkerSizeData is not None:
-        img, dist, markerSize = precalibrateMarkerSizeData
-        marker_size = calibrateMarkerSize(img, markerSize, dist, at_detector, camera_fc_params)
+MARKER_SIZE = 0.06979
+at_detector = Detector(
+    families="tagStandard41h12",
+    nthreads=4,
+    quad_decimate=1.0,
+    quad_sigma=0.0,
+    refine_edges=1,
+    decode_sharpening=0.25,
+    debug=0
+)
 
-    if type(source) is str and source.endswith(".jpg"):
-        processImage(cv2.imread(source), at_detector, camera_matrix, dist_coeffs, camera_fc_params)
-        return
-
-    # Capture video from camera
-    cap = cv2.VideoCapture(source)
-    firstFrame = True
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
+def findMarksOnFrame(frame, cameraFCParams, markerSize):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         detectionResult = at_detector.detect(
-            gray, estimate_tag_pose=True, camera_params=camera_fc_params, tag_size=marker_size)
+            gray, estimate_tag_pose=True, camera_params=cameraFCParams, tag_size=markerSize)
 
         ids = []
         corners = []
@@ -88,30 +35,71 @@ def startRecognize(camera_config, source, recognitionProcessor, precalibrateMark
             rvecs.append(result.pose_R)
             tvecs.append(result.pose_t)
 
-        if ids != []:
-            for i in range(len(ids)):
-                # Calculate distance
-                distance = np.linalg.norm(tvecs[i])
+        return ids, corners, tvecs, rvecs
 
-                recognitionProcessor.process({
-                    "distance": distance,
-                    "tvec" : tvecs[i].T[0],
-                    "rvec" : cv2.Rodrigues(rvecs[i]).T[0],
-                    "id": ids[i]
-                })
-                cv2.drawFrameAxes(frame, camera_matrix, dist_coeffs,
-                                  rvecs[i], tvecs[i], marker_size * 0.5)
-                cv2.putText(frame, f"Dist: {distance:.2f}m x: {tvecs[i][0][0]:.2f} y: {tvecs[i][1][0]:.2f} z: {tvecs[i][2][0]:.2f}", (
-                    10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-                cv2.putText(
-                    frame, f"Id: {ids[i]}", (100, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
-        cv2.imshow('Frame', frame)
-        if (firstFrame):
-            cv2.waitKey(0)
-            firstFrame = False
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+def processFrame(frame, camInfo: CameraInfo, markerSize, recognitionProcessor, waitDelay = 1):
+    ids, corners, tvecs, rvecs = findMarksOnFrame(frame, camInfo.fcParams, markerSize)
+    if ids != []:
+        for i in range(len(ids)):
+            # Calculate distance
+            distance = np.linalg.norm(tvecs[i])
+            response = recognitionProcessor.process({ # TODO: send all detected marks by one message
+                "distance": distance,
+                "tvec" : tvecs[i].T[0],
+                "rvec" : list(cv2.Rodrigues(rvecs[i]))[0].T[0],
+                "id": ids[i]
+            })
+            if response is not None and response != {}:
+                if response["type"] != "DrawObjectBox":
+                    print(f"Undefined response type. Skipping...")
+                else:
+                    name, size, pos = response["name"], np.asarray(response["size"]), np.asarray(response["pos"]).reshape((3, 1))
+                    pos = tvecs[i] 
+                    rot, _ = cv2.Rodrigues(np.asarray(response["rot"]).reshape((3, 1)))
+                    rot = rvecs[i]
+
+                    DF.drawMarkFrame(frame, corners[i])
+                    rot = np.asarray([1 if i == j else 0 for i in range(3) for j in range(3)], dtype=np.float64).reshape((3, 3))
+                    angles = DF.calculateAnglesPos(pos, size, rot, camInfo)
+                    DF.drawBoundingBoxOnFrame(frame, angles)
+                    DF.drawNameAboveMarker(frame, name, tvecs[i], camInfo)
+
+            cv2.drawFrameAxes(frame, camInfo.matrix, camInfo.distCoeffs, rvecs[i], tvecs[i], markerSize * 0.5)
+    frame = cv2.resize(frame, (1000, 500))
+    cv2.imshow('Frame', frame)
+    cv2.waitKey(waitDelay)
+
+
+def startRecognize(cameraConfig, source, recognitionProcessor, precalibrateMarkerSizeData = None):
+    """
+        source is a camera id or file's name is containing video
+        source may be also image file
+        if precalibrateMarkerSizeData as (img with marker, dist to marker, real marker size) is given, marker size will be precalibrated
+    """
+    cameraMatrix, distCoeffs, tvec, rvec = cameraConfig["cameraMatrix"], cameraConfig["distCoeffs"], cameraConfig["tvec"], cameraConfig["rvec"]
+    cameraFCParams = np.asarray([cameraConfig["fx"], cameraConfig["fy"],
+                        cameraConfig["cx"], cameraConfig["cy"]])
+    cameraInfo = CameraInfo(cameraMatrix, distCoeffs, tvec, rvec, cameraFCParams)
+
+    markerSize = MARKER_SIZE
+    if precalibrateMarkerSizeData is not None:
+        img, dist, markerSize = precalibrateMarkerSizeData
+        markerSize = calibrateMarkerSize(img, markerSize, dist, at_detector, cameraFCParams)
+
+    if type(source) is str and source.endswith(".jpg"):
+        processFrame(cv2.imread(source), cameraInfo, markerSize,recognitionProcessor)
+        cv2.waitKey(0)
+        return
+
+    # Capture video from camera
+    cap = cv2.VideoCapture(source)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
             break
+
+        processFrame(frame, cameraInfo,markerSize, recognitionProcessor)
 
     cap.release()
     cv2.destroyAllWindows()

ArucoDistance/CameraCalibrator.py
@@ -8,70 +8,9 @@ criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 EVALUATION_ERROR = 0.001
 
-
-def calibrateUsingCamera(cameraIndex=0, dirToStoreGoodCalibrationFrames=None, doLog=False):
-    gridHeight = 7
-    gridWidth = 5
-    print("Calibrating started please keep chessboard image opposite camera")
-    objp = np.zeros((gridHeight*gridWidth, 3), np.float32)
-    objp[:, :2] = np.mgrid[0:gridHeight, 0:gridWidth].T.reshape(-1, 2)
-
-    # Arrays to store object points and image points from all the images.
-    objpoints = []  # 3d point in real world space
-    imgpoints = []  # 2d points in image plane.
-
-    gray = None
-    cap = cv.VideoCapture(cameraIndex)
-    goodFrames = 0
-    while goodFrames < 15:
-        ret, img = cap.read()
-        if not ret:
-            print("Warn: bad camera")
-        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-        # Find the chess board corners
-        ret, corners = cv.findChessboardCorners(
-            gray, (gridHeight, gridWidth), None)
-
-        # If found, add object points, image points (after refining them)
-        if ret == True:
-            goodFrames += 1
-            objpoints.append(objp)
-
-            corners2 = cv.cornerSubPix(
-                gray, corners, (11, 11), (-1, -1), criteria)
-            imgpoints.append(corners2)
-
-            if (doLog):
-                print("The image good for calibration")
-            if (dirToStoreGoodCalibrationFrames is not None):
-                cv.imwrite(
-                    f"{dirToStoreGoodCalibrationFrames}/{goodFrames}.jpg", img)
-        cv.imshow('Frame', img)
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
-        time.sleep(0.5)
-  
-    cv.destroyAllWindows()
-
-    if gray is None:
-        print("Error in calibrateUsingCamera: Bad images, can't calibrate")
-        return {}
-    mtx_fake = np.zeros((3, 3))
-    dist_fake = np.zeros((1, 5))
-    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(
-        objpoints, imgpoints, gray.shape[::-1], mtx_fake, dist_fake, None, None, 0, criteria)
-    return {
-        "camera_matrix": mtx,
-        "distortion": dist,
-        "rvecs": rvecs,
-        "tvecs": tvecs,
-    }
-
-
 def calibrateUsingImages(dirWithImages):
-    objp = np.zeros((7*7, 3), np.float32)
-    objp[:, :2] = np.mgrid[0:7, 0:7].T.reshape(-1, 2)
+    objp = np.zeros((9*6, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:6, 0:9].T.reshape(-1, 2)
 
     # Arrays to store object points and image points from all the images.
     objpoints = []  # 3d point in real world space
@@ -84,7 +23,7 @@ def calibrateUsingImages(dirWithImages):
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
         # Find the chess board corners
-        ret, corners = cv.findChessboardCorners(gray, (7, 7), None)
+        ret, corners = cv.findChessboardCorners(gray, (6, 9), None)
 
         # If found, add object points, image points (after refining them)
         if ret == True:
@@ -99,13 +38,19 @@ def calibrateUsingImages(dirWithImages):
         return {}
     mtx_fake = np.zeros((3, 3))
     dist_fake = np.zeros((1, 5))
-    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(
-        objpoints, imgpoints, gray.shape[::-1], mtx_fake, dist_fake, None, None, 0, criteria)
+    ret, mtx, dist, _, _ = cv.calibrateCamera(
+        objpoints, imgpoints, gray.shape, mtx_fake, dist_fake, None, None, 0, criteria)
+    ret, rvec, tvec = cv.solvePnP(np.array(objpoints[0]),np.array(imgpoints[0]), mtx, dist, None, None, flags=cv.SOLVEPNP_EPNP)
+
     return {
-        "camera_matrix": mtx,
-        "distortion": dist,
-        "rvecs": rvecs,
-        "tvecs": tvecs,
+        "cameraMatrix": mtx,
+        "distCoeffs": dist,
+        "rvec": rvec,
+        "tvec": tvec,
+        "fx": mtx[0][0],
+        "fy": mtx[1][1],
+        "cx": mtx[0][2],
+        "cy": mtx[1][2],
     }
 
 def calibrateMarkerSize(imgWithMark, realMarkerSize, realDistanceToMarker, detector, camera_fc_params, eps=EVALUATION_ERROR):

ArucoDistance/utils.py
@@ -20,13 +20,24 @@ def loadCameraConfig(path: str):
     if path.endswith(".json"):
         with open(path, "r") as file:
             camera_config = json.load(file)
-        camera_config["camera_matrix"] = np.asarray(
-            camera_config["camera_matrix"])
-        camera_config["distortion"] = np.asarray(camera_config["distortion"])
-        camera_config["rvecs"] = np.asarray(camera_config["rvecs"])
-        camera_config["tvecs"] = np.asarray(camera_config["tvecs"])
+        camera_config["cameraMatrix"] = np.asarray(
+            camera_config["cameraMatrix"])
+        camera_config["distCoeffs"] = np.asarray(camera_config["distCoeffs"])
+        camera_config["rvec"] = np.asarray(camera_config["rvec"])
+        camera_config["tvec"] = np.asarray(camera_config["tvec"])
         return camera_config
     elif path.endswith(".yaml"):
         with open(path, "r") as file:
             camera_config = yaml.safe_load(file)
         return camera_config
+
+class CameraInfo:
+    def __init__(self, matrix, distCoeffs, tvec, rvec, fcParams = None):
+        self.matrix = matrix
+        self.distCoeffs = distCoeffs
+        self.tvec = tvec
+        self.rvec = rvec
+        if fcParams is None:
+            self.fcParams = np.asarray([matrix[0][0], matrix[1][1], matrix[0][2], matrix[1][2]])
+        else:
+            self.fcParams = fcParams

Server/Server.py
@@ -26,14 +26,21 @@ def notifyRevitAboutMovedObject(guid, newPos, newRot, revitSocket):
     
 
 class Server:
-    def __init__(self, context, serverSocket, revitSocket, dbAdapter: IDataBaseAdapter):
+    def __init__(self, context, server2CameraSocket, revit2ServerSocket, server2RevitSocket, dbAdapter: IDataBaseAdapter):
         self.context = context
+
+        self.server2CameraSocket = context.socket(zmq.REP)
+        self.server2CameraSocket.connect(server2CameraSocket)
         
-        self.socket = context.socket(zmq.PULL)
-        self.socket.connect(serverSocket)
+        self.revit2ServerSocket = context.socket(zmq.PULL)
+        self.revit2ServerSocket.connect(revit2ServerSocket)
+
+        self.poller = zmq.Poller()
+        self.poller.register(self.server2CameraSocket, zmq.POLLIN)
+        self.poller.register(self.revit2ServerSocket, zmq.POLLIN)
         
-        self.revitSocket = context.socket(zmq.PUSH)
-        self.revitSocket.connect(revitSocket)
+        self.server2RevitSocket = context.socket(zmq.PUSH)
+        self.server2RevitSocket.connect(server2RevitSocket)
 
         self.dbAdapter = dbAdapter
 
@@ -41,6 +48,8 @@ class Server:
 
     def processDetectedMark(self, data):
         tagId, tvec, rvec, cameraId = data["id"], np.asarray(data["tvec"]), np.asarray(data["rvec"]), data["cameraId"]
+        if cameraId not in self.camerasPositionData:
+            self.camerasPositionData[cameraId] = np.array([0.0, 0.0, 0.0])
 
         objectData = self.dbAdapter.getData(tagId)
         if objectData is None:
@@ -48,21 +57,32 @@ class Server:
             return
         
         if objectData.isPositional:
-            self.camerasPositionData[cameraId] = [np.asarray(tvec) + np.asarray(objectData.markPos), rvec]
+            cameraRot, _ = cv2.Rodrigues(rvec)
+            self.camerasPositionData[cameraId] = np.dot(cameraRot, np.asarray(objectData.markPos) - np.asarray(tvec))
+            print(f"Position of camera with id: {cameraId} has updated")
             return
 
         oldPos = objectData.markPos
         if isObjectMoved(oldPos, tvec):
-            objectData.markPos = self.camerasPositionData[cameraId][0] + tvec
+            objectData.markPos = self.camerasPositionData[cameraId] + tvec
+            # self.dbAdapter.updateData(objectData)
+            # notifyRevitAboutMovedObject(objectData.guid, tvec, rvec, self.revitSocket) ## Rewrite
             
-            cameraRotMatrix, _ = cv2.Rodrigues(self.camerasPositionData[cameraId][1])
-            markRotMatrix, _ = cv2.Rodrigues(rvec)
-            resultRotMatrix = cameraRotMatrix @ markRotMatrix
-            resultRot = cv2.Rodrigues(resultRotMatrix)
+        cameraRotMatrix, _ = cv2.Rodrigues(self.camerasPositionData[cameraId])
+        markRotMatrix, _ = cv2.Rodrigues(rvec)
+        markObjVector = objectData.objectPos - objectData.markPos
+        objectPos = tvec + markObjVector
 
-            objectData.rotation_ = resultRot
-            self.dbAdapter.updateData(objectData)
-            notifyRevitAboutMovedObject(objectData.guid(), tvec, rvec, self.revitSocket)
+        recv = {
+                "type" : "DrawObjectBox",
+                "name" : objectData.name,
+                "size" : list(objectData.size if objectData.size is not None else [0, 0, 0]),
+                "pos" : list(objectPos.reshape(3)),
+                "rot" : list(rvec.reshape(3))
+                }
+            
+            
+        return recv
 
     def processPostElementData(self, data):
         guid = data['guid']
@@ -93,15 +113,25 @@ class Server:
     def process(self, message):
         data = json.loads(message)
         print(data)
+        recv = None
         if data["type"] == "DetectedMark":
-            self.processDetectedMark(data)
+            recv = self.processDetectedMark(data)
         elif data["type"] == "PostElementData":
             self.processPostElementData(data);
+
+        return recv
 
         
     
     def startProccessing(self):
         print("Message proccessing started")
         while True:
-            message = self.socket.recv()
-            self.process(message)
+            sockets = dict(self.poller.poll())
+
+            if self.revit2ServerSocket in sockets:
+                self.process(self.revit2ServerSocket.recv_string())
+            if self.server2CameraSocket in sockets:
+                recv = self.process(self.server2CameraSocket.recv_string())
+                recv = {} if recv is None else recv
+                self.server2CameraSocket.send_string(json.dumps(recv))
+